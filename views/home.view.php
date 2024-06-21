a9d80fa1b8d36b96da47909a973ef3b3c7e31666
behnamhamidi1991 <eslp91@hotmail.com>
2024-06-21 20:43:04 +0330

    first commit

---
+++ views/home.view.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="./style.css">
+    <title>DevAcademy</title>
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header id="header" class="header">
+        <div class="logoContainer">
+            <a href="#" class="logo">
+                <i class='bx bx-code'></i>
+                <span>DevAcademy</span>
+            </a>
+        </div>
+
+        <div class="nav-links">
+            <ul class="navigation-box">
+                <li class="nav-item">
+                    <a href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">Courses</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="header-buttons">
+            <div class="user-buttons">
+                <a href="#" class="loginBtn">Login</a>
+                <a href="#" class="registerBtn">Register</a>
+            </div>
+            <!-- <div class="blog-button">
+                <a href="#" class="createPost">Create Post</a>
+                <a href="#" class="logoutBtn">Logout</a>
+            </div> -->
+        </div>
+    </header>
+
+
+
+    <!-- HOME TOP -->
+    <section id="home-top">
+        <div class="home-top-content">
+            <h1>DevAcademy</h1>
+            <p>One place you can find all about web and app development ...</p>
+            <div class="home-top-btns">
+                <button>Read More</button>
+                <button>Support</button>
+            </div>
+
+        </div>
+    </section>
+
+
+    <section id="notes">
+        <div class="note-box">
+            <i class='bx bx-code-alt'></i>
+            <h4>Our Courses</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero hic illum ab incidunt, porro repellat
+                voluptas a reiciendis consequuntur quidem dicta?</p>
+        </div>
+
+        <div class="note-box">
+            <i class='bx bxs-wallet-alt'></i>
+            <h4>Secure Payment</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero hic illum ab incidunt, porro repellat
+                voluptas a reiciendis consequuntur quidem dicta?</p>
+        </div>
+
+        <div class="note-box">
+            <i class='bx bxl-visual-studio'></i>
+            <h4>Free Applications</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero hic illum ab incidunt, porro repellat
+            voluptas a reiciendis consequuntur quidem dicta?</p>
+        </div>
+
+        <div class="note-box">
+            <i class='bx bxs-graduation'></i>
+            <h4>Valid Degree</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero hic illum ab incidunt, porro repellat
+            voluptas a reiciendis consequuntur quidem dicta?</p>
+        </div>
+    </section>
+
+
+    <section id="blog">
+        <div class="blog-content">
+            <div class="blog-box">
+                <h2>JavaScript: First step for Frontend</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
+                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
+                    tenetur enim, obcaecati, autem ipsum?</p>
+                <div class="post-details">
+                    <span>11/02/2023</span>
+                    <span>Adam Divine</span>
+                    <span>Frontend</span>
+                </div>
+                <a href="#" class="blog-readmore-btn">Read More</a>
+            </div>
+
+            <div class="blog-box">
+                <h2>PHP new features</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
+                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
+                    tenetur enim, obcaecati, autem ipsum?</p>
+                <div class="post-details">
+                    <span>18/02/2023</span>
+                    <span>Rebecca Albert</span>
+                    <span>Backend</span>
+                </div>
+                <a href="#" class="blog-readmore-btn">Read More</a>
+            </div>
+
+            <div class="blog-box">
+                <h2>Your ideal laptop for development</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
+                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
+                    tenetur enim, obcaecati, autem ipsum?</p>
+                <div class="post-details">
+                    <span>18/02/2023</span>
+                    <span>Rebecca Albert</span>
+                    <span>Backend</span>
+                </div>
+                <a href="#" class="blog-readmore-btn">Read More</a>
+            </div>
+
+            <div class="blog-box">
+                <h2>Why is Java Famous in Germany?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
+                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
+                    tenetur enim, obcaecati, autem ipsum?</p>
+                <div class="post-details">
+                    <span>11/02/2023</span>
+                    <span>Adam Divine</span>
+                    <span>Frontend</span>
+                </div>
+                <a href="#" class="blog-readmore-btn">Read More</a>
+            </div>
+
+            <div class="blog-box">
+                <h2>What is the main reason for errors?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
+                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
+                    tenetur enim, obcaecati, autem ipsum?</p>
+                <div class="post-details">
+                    <span>11/02/2023</span>
+                    <span>Adam Divine</span>
+                    <span>Frontend</span>
+                </div>
+                <a href="#" class="blog-readmore-btn">Read More</a>
+            </div>
+
+            <div class="blog-box">
+                <h2>Who can find a new job?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore perspiciatis modi, nostrum
+                    asperiores itaque voluptates nisi omnis magnam sunt excepturi ratione dolorum commodi ut accusamus
+                    tenetur enim, obcaecati, autem ipsum?</p>
+                <div class="post-details">
+                    <span>11/02/2023</span>
+                    <span>Adam Divine</span>
+                    <span>Frontend</span>
+                </div>
+                <a href="#" class="blog-readmore-btn">Read More</a>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section id="searchbar">
+        <form>
+            <p>Are you looking for something specific?</p>
+            <input type="text" placeholder="Search here ...">
+            <button>
+                <i class='bx bx-search-alt-2'></i>
+            </button>
+        </form>
+
+    </section>
+
+
+
+    <footer id="footer">
+        <h2>DevAcademy</h2>
+        <p>Find your ideal course with us</p>
+        <p>Copyright 2024</p>
+    </footer>
+
+</body>
+
+</html>
