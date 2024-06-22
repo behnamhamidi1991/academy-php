1e04ca16e5dc7d8538e56fa13cb155bf1c0cb8c5
behnamhamidi1991 <eslp91@hotmail.com>
2024-06-22 14:26:05 +0330

    routes

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 require '../helpers.php';
-loadView('home');
+
+$routes = [
+    '/' => 'controllers/home.php',
+    '/blog' => 'controllers/blog/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
+
+
+
