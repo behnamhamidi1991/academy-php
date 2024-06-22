a3fbbcba0c74d79cfa79a3009c7ccd81e72610f9
behnamhamidi1991 <eslp91@hotmail.com>
2024-06-22 14:31:39 +0330

    controllers added

--- public/index.php
+++ public/index.php
@@ -9,4 +9,11 @@ $routes = [
 ];
 
 
+$uri = $_SERVER['REQUEST_URI'];
+
+if (array_key_exists($uri, $routes)) {
+    require(basePath($routes[$uri]));
+} else {
+    require basePath($routes['404']);
+}
 
