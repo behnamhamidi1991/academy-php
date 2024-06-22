1c7ae0c4f16cfc646dad7dfe6a96313a0d3d528f
Behnam Hamidi <eslp91@hotmail.com>
2024-06-22 23:18:27 +0330

    update routes

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require '../helpers.php';
 $routes = [
     '/' => 'controllers/home.php',
     '/blog' => 'controllers/blog/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
+    '/blog/create' => 'controllers/blog/create.php',
+    '404' => 'controllers/error/404.php',
 ];
 
 
