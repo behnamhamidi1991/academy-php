7c4cc6eab9739db45060e637e2437f3901a953ef
behnamhamidi1991 <eslp91@hotmail.com>
2024-06-23 09:34:49 +0330

    let's merge the changes

--- views/blog/create.view.php
+++ views/blog/create.view.php
@@ -1,3 +1,31 @@
-<?php 
+<?php loadPartial('head') ?>
+<?php loadPartial('navbar') ?>
 
-echo "You can create a post here ...";
+
+    <!-- FORM -->
+     <section class="form-wrapper">
+         <form id="create-form">
+             <h2>Create a new post</h2>
+             <p>Write what is in your mind ...</p>
+             
+             <input type="text" placeholder="Title ...">
+             <input type="text" placeholder="Category ...">
+             <textarea placeholder="Content ..."></textarea>
+          </form>
+      </section>
+
+    <footer id="footer">
+        <h2>DevAcademy</h2>
+        <p>Find your ideal course with us</p>
+        <p>Copyright 2024</p>
+    </footer>
+
+
+
+
+</body>
+
+</html>
+
+
+<?php loadPartial('footer') ?>
