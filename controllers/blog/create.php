f22f1195f4a2dc782c7f754cf0a08d71fc0964a0
behnamhamidi1991 <eslp91@hotmail.com>
2024-06-24 09:11:50 +0330

    Start over

--- controllers/blog/create.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-<<<<<<< HEAD
-
-=======
->>>>>>> c9b4d6ccceb0207ea455c750a938fe0710d06494
-loadView('blog/create');
--- controllers/blog/index.php
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<?php
-
-loadView('blog/index');
-=======
-<?php 
-
-loadView('blog/index');
->>>>>>> c9b4d6ccceb0207ea455c750a938fe0710d06494
